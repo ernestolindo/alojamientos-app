ba7423493f52da132d6e67d9a432feec03774759
ernestolindo <w.ernestolindo@gmail.com>
2025-01-10 15:01:28 -0600

    Eliminado archivo de desarrollo

--- hashes-gen.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo password_hash('adminpass', PASSWORD_DEFAULT) . '<br>';
-echo password_hash('adminpass', PASSWORD_DEFAULT) . '<br>';
-echo password_hash('adminpass', PASSWORD_DEFAULT) . '<br>';
-echo '<br>';
-echo password_hash('password123', PASSWORD_DEFAULT) . '<br>';
-echo password_hash('password123', PASSWORD_DEFAULT) . '<br>';
-echo password_hash('password123', PASSWORD_DEFAULT) . '<br>';
